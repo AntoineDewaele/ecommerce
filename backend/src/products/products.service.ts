df4916b96215d1f92d205dcbfe5fdef3e3582a9f
Antoine Dewaele <antoinee.dewaele@gmail.com>
2023-09-20 16:49:53 +0200

    add generic crud service and pagination system

--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Product } from './models/product.model';
-import { CrudService } from '../base.service';
+import { CrudService } from '../crud.service';
 
 @Injectable()
 export class ProductsService extends CrudService<Product>(Product, 'product') {}
